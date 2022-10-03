e55994803e9c0f7ee0543d43370935b6706a802a
vifieh <vifiehruth@gmail.com>
2022-10-03 21:45:06 +0100

    completing project

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a2eab4d4107a546389b6630a8ab63ee5-1746794995.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
