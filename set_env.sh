d5c02d5a68782709032947313abf71183b9c2f32
vifieh <vifiehruth@gmail.com>
2022-10-03 08:57:00 +0100

    removed errors in files

--- set_env.sh
+++ set_env.sh
@@ -2,8 +2,8 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=database_dev
 export POSTGRES_PASSWORD=database_dev
-export POSTGRES_HOST=database-513081378057-dev.cseiaiwbhzsr.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=database-513081378057-dev
+export POSTGRES_HOST=database-dev.ckrbas7xact3.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=database-dev
 export AWS_BUCKET=arn:aws:s3:::test-401209005059-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
